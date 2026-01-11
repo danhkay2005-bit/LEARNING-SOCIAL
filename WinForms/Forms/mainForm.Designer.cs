5f9fa09b24965eca19d43190a062da024caad52a
danhkay2005-bit <danhkay2005@gmail.com>
2026-01-12 05:43:09 +0700

    .

--- WinForms/Forms/mainForm.Designer.cs
+++ WinForms/Forms/mainForm.Designer.cs
@@ -1,39 +1,59 @@
-﻿namespace WinForms.Forms
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WinForms.Forms
 {
-    partial class MainForm
+    public partial class MainForm : Form
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
-        private System.ComponentModel.IContainer components = null;
-
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing && (components != null))
-            {
-                components.Dispose();
-            }
-            base.Dispose(disposing);
-        }
-
-        #region Windows Form Designer generated code
-
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "MainForm";
+            splitContainer1 = new SplitContainer();
+            splitContainer2 = new SplitContainer();
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).BeginInit();
+            splitContainer1.Panel2.SuspendLayout();
+            splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)splitContainer2).BeginInit();
+            splitContainer2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            splitContainer1.Dock = DockStyle.Fill;
+            splitContainer1.Location = new Point(0, 0);
+            splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel2
+            // 
+            splitContainer1.Panel2.Controls.Add(splitContainer2);
+            splitContainer1.Size = new Size(1129, 483);
+            splitContainer1.SplitterDistance = 192;
+            splitContainer1.TabIndex = 0;
+            // 
+            // splitContainer2
+            // 
+            splitContainer2.Dock = DockStyle.Fill;
+            splitContainer2.Location = new Point(0, 0);
+            splitContainer2.Name = "splitContainer2";
+            splitContainer2.Size = new Size(933, 483);
+            splitContainer2.SplitterDistance = 751;
+            splitContainer2.TabIndex = 0;
+            // 
+            // MainForm
+            // 
+            ClientSize = new Size(1129, 483);
+            Controls.Add(splitContainer1);
+            Name = "MainForm";
+            splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).EndInit();
+            splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)splitContainer2).EndInit();
+            splitContainer2.ResumeLayout(false);
+            ResumeLayout(false);
+
         }
 
-        #endregion
+        private SplitContainer splitContainer1;
+        private SplitContainer splitContainer2;
     }
 }
