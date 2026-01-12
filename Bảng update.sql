6354a948188ab15f80209c9d9e3a101ef19fa64b
danhkay2005-bit <danhkay2005@gmail.com>
2026-01-12 16:30:26 +0700

    .

--- Bảng update.sql
+++
@@ -1,6 +0,0 @@
-SELECT * FROM CapDo;
-
-INSERT INTO CapDo (TenCapDo, MucXPToiThieu, MucXPToiDa)
-VALUES (N'Level 1', 0, 100);
-SELECT TOP 10 MaCapDo, TenCapDo FROM CapDo ORDER BY MaCapDo;
-SELECT * FROM NguoiDung;
